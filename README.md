f52b1eaecb717091b3d2bdaced7fdfcb3fca427d
Carlonii <dipradofilipe@gmail.com>
2024-03-18 15:48:45 -0300

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # Spring-Web-Api-and_Swagger
+
+API WEB desenvolvida no curso de Spring Boot da DIA para aprimorar meus conhecimentos 
---
+++ src/main/java/dio/web/api/doc/SwaggerConfig.java
@@ -0,0 +1,7 @@
+import java.util.Arrays;
+import java.util.HashSet;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+
